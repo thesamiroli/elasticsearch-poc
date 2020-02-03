3da4c1f72ce324c89f627a6ec8832f02741f6a6f
Samir Oli <samiroli789@gmail.com>
2020-02-03 22:34:41 +0545

    basic filtering done

--- elastic/elastic-operations.js
+++ elastic/elastic-operations.js
@@ -1,7 +1,6 @@
 const elasticClient = require("./elastic-config").client;
 
 async function getAllPhilosophers(req, res, next) {
-  console.log("Get all Philosophers");
   let data = await elasticClient.search({
     index: "philosophers"
   });
@@ -9,35 +8,43 @@ async function getAllPhilosophers(req, res, next) {
 }
 
 async function search(req, res, next) {
-  console.log("Search----------------------");
-  console.log(req.body);
+  if (req.body.filters.sortBy === "name")
+    var sortBy = req.body.filters.sortBy + ".keyword";
+  else if (req.body.filters.sortBy === "relevance") var sortBy = "_score";
+  else var sortBy = req.body.filters.sortBy;
   let data = {};
 
-  if (req.body.searchBy === "everything") {
-    data = await elasticClient.search({
-      index: "philosophers",
-      body: {
-        query: {
-          multi_match: {
-            query: req.body.searchTerm,
-            fields: ["name", "books", "livedIn", "summary"]
-          }
+  if (req.body.filters.searchOn === "all") {
+    data = await elasticClient
+      .search({
+        index: "philosophers",
+        body: {
+          query: {
+            multi_match: {
+              query: req.body.searchTerm,
+              fields: ["name", "books", "livedIn", "summary"]
+            }
+          },
+          sort: [{ [sortBy]: req.body.filters.sortType }]
         }
-      }
-    });
+      })
+      .catch(err => console.log("Err", err.meta.body.error));
+    res.status(200).json(data.body.hits.hits);
   } else {
-    data = await elasticClient.search({
-      index: "philosophers",
-      body: {
-        query: {
-          match: {
-            [req.body.searchBy]: req.body.searchTerm
+    data = await elasticClient
+      .search({
+        index: "philosophers",
+        body: {
+          query: {
+            match: {
+              [req.body.filters.searchOn]: req.body.searchTerm
+            }
           }
         }
-      }
-    });
+      })
+      .catch(err => console.log("Err", err));
+    res.status(200).json(data.body.hits.hits);
   }
-  res.status(200).json(data.body.hits.hits);
 }
 
 module.exports = {
